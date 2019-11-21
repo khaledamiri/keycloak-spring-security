433abd64a8e3e2b16a727dd6f09c581d539fc712
Khaled <khaled-amiri@hotmail.fr>
2019-11-21 21:39:46 +0100

    first commit

---
+++ src/test/java/com/amiri/springsecuritykeycloak/SpringSecurityKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amiri.springsecuritykeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
